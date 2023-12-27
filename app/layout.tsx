84e3fc64ff64503eca2cb10ecddad80cc75d82da
Rodelo Escueta <rodelo.escueta@outgive.ca>
2023-12-27 17:06:16 +0800

    change bg gradiant

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[rgb(226,251,245)] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[rgb(98,148,116)]"></div>
-        <div className="bg-[rgb(247,215,251)] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[rgb(144,99,148)]"></div>
-
+        <div className="bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-sky-300 via-sky-200 to-sky-100 dark:from-gray-900 dark:via-sky-800 dark:to-gray-400 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
